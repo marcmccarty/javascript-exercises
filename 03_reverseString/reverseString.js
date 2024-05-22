3fc99cf925ae1d3b1927ff95adffa5a7841ebd9e
Marc McCarty <maruku115@gmail.com>
2024-05-22 15:36:24 -0500

    Solve 3rd js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reversedStringArray = stringArray.reverse();
+    let reversedString = "";
+    for (let char of stringArray) {
+        reversedString += char;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
