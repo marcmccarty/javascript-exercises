a89363f20660ea83a7b174b07784550b8c568865
Marc McCarty <maruku115@gmail.com>
2024-05-22 16:17:38 -0500

    Solve 5th & 6th js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(firstValue, secondValue) {
+    let sum = 0;
+    let minimumValue;
+    let maximumValue;
 
+    if (typeof(firstValue) != "number" || typeof(secondValue) != "number") {
+        return 'ERROR';
+    }
+
+    if (firstValue < 0 || secondValue < 0) {
+        return 'ERROR';
+    }
+
+    if (firstValue < secondValue) {
+        minimumValue = firstValue;
+        maximumValue = secondValue;
+    } else if (firstValue > secondValue) {
+        minimumValue = secondValue;
+        maximumValue = firstValue;
+    } else {
+        minimumValue = firstValue;
+        maximumValue = minimumValue;
+    }
+
+    for (i = minimumValue; i <= maximumValue; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
