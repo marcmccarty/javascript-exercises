d4edadc20e5ce23605997e106e823670b9d13b25
Marc McCarty <maruku115@gmail.com>
2024-05-22 15:56:03 -0500

    Solve 4th js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (item of itemsToRemove) {
+        for (let i = 0; i < array.length; i++) {
+            if (item === array[i]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
