387fc105649f330f91da694a07a8659620a5c6fd
Marc McCarty <maruku115@gmail.com>
2024-06-05 15:06:59 -0500

    Solve 10th js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (parseInt(num) < 0) {
+        return "OOPS"
+    } else if (parseInt(num) == 0) {
+        return 0;
+    } else if (parseInt(num) == 1 || parseInt(num) == 2) {
+        return 1;
+    } else {
+        return fibonacci(parseInt(num) - 1) + fibonacci(parseInt(num) - 2);
+    }
 };
 
 // Do not edit below this line
