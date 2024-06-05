a4e4b51c431a8ffe3b993f82c35be0ec1f477e0c
Marc McCarty <maruku115@gmail.com>
2024-06-05 14:58:53 -0500

    Solve 9th js exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let strippedPalindrome = string
+    .split(' ').join('')
+    .split('!').join('')
+    .split(',').join('')
+    .split('.').join('')
+    .toLowerCase();
+    if (strippedPalindrome == reverseString(strippedPalindrome)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// Alternative Solution:
+// Create a new reversed string for comparison
+// const reversedString = cleanedString.split('').reverse().join('');
+function reverseString(string) {
+    let reversedString = "";
+    for (i = string.length -1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
