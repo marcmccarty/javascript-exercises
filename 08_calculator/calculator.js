dd988bd48023216f072d3a4be5779bda1478845c
Marc McCarty <maruku115@gmail.com>
2024-06-05 14:46:37 -0500

    Solve 8th js exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	solution = base;
+  for (i = 1; i < exponent; i++) {
+    solution *= base;
+  }
+  return solution;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	solution = 1;
+  if (num == 0 || num == 1) {
+    return solution;
+  } else {
+    for (i = 2; i <= num; i++) {
+      solution = i * factorial(i-1);
+    }
+  }
+  return solution;
 };
 
 // Do not edit below this line
